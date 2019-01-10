915ed8f144e61dae892707920a9b43e0327335df
mowings1 <mowings1@tulane.edu>
2019-01-10 12:23:00 -0600

    refactoring
    passed models into seperate directory, added initial route for todos, setup on port 3000

--- server/server.js
+++ server/server.js
@@ -1,54 +1,25 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
-
-
-// var newTodo = new Todo({
-//   text: true
-// });
-//
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo: ', doc)
-// }, (e) => {
-//   console.log('Unable to save todo', e);
-// });
-
-// user model
-// email - required and trimmed, string, min length of 1
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
-});
-
-var User = new User({
-  email: "silas@tulane.edu"
-});
-
-User.save().then((doc) => {
-  console.log('Saved new user: ', doc);
-}, (e) => {
-console.log('Unable to save new user', e);
+app.listen(3000, () => {
+  console.log('Running on port 3000');
 });
